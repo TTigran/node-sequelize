084cdd41c105b7dd8b5976bce797b7754883d719
tigran <martirosyantigran.developer@gmail.com>
2021-04-22 09:04:01 +0400

    change db to postgres

--- models/index.js
+++ models/index.js
@@ -2,6 +2,7 @@ const config = require('../config').test;
 const Sequelize = require("sequelize");
 
 const {name, username, password, host, database, dialect} = config;
+console.log("dialect")
 const sequelize = new Sequelize(name, username, password, {
     host,
     dialect,
@@ -17,7 +18,7 @@ db.sequelize = sequelize;
 db.products = require("./products.js")(sequelize, Sequelize);
 db.users = require("./users.js")(sequelize, Sequelize);
 
-db.users.hasOne(db.products, );
-db.products.belongsTo(db.users, );
+db.users.hasOne(db.products);
+db.products.belongsTo(db.users);
 
 module.exports = db;
